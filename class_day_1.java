2837bab06f0279fa33b146a7fbbcc1db1a68b15d
howard1688 <111676522+howard1688@users.noreply.github.com>
2022-09-30 09:11:19 +0800

    Add files via upload

---
+++ class_day_1.java
@@ -0,0 +1,10 @@
+public class class_day_1{
+	public static void main(String[] args){
+	System.out.println(args[0]);
+	printStr("aaaaaaa");
+	}
+	
+	public static void printStr(String str){
+System.out.println("aaaaa"+str);
+	}
+}
