70ce183d61d137217e6674989436a78ac2c071cb
howard1688 <111676522+howard1688@users.noreply.github.com>
2022-10-07 10:19:06 +0800

    Add files via upload

---
+++ c21.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class c21{
+	public static void main(String[] args){
+        int grade;
+        Scanner input= new Scanner(System.in);
+        System.out.println("輸入成績1");
+        grade= input.nextInt();
+        if(grade>=101&grade<=109){
+            System.out.println("不要亂打北七");
+        }
+        else{
+            switch(grade/10)
+                {   
+                case 10:
+                case 9:
+                    System.out.println('A');
+                    break;
+                case 8:
+                    System.out.println('B');
+                    break;
+                case 7:
+                    System.out.println('C');
+                    break;
+                case 6:
+                    System.out.println('D');
+                    break;
+                case 5,4,3,2,1,0:
+                    System.out.println('E');
+                    break;
+                default:
+                    System.out.println("不要亂打北七");
+                    break;
+        
+            }
+        }
+    }
+}
