29ac8448a06a158ad1cff5b42c7ac7f4a227c699
howard1688 <111676522+howard1688@users.noreply.github.com>
2022-10-07 08:30:48 +0800

    Add files via upload

--- class_day_1.java
+++ class_day_1.java
@@ -13,15 +13,24 @@ public class class_day_1{
         n2 = input.nextInt();
         sum=(n1+n2)/2;
         if(sum>=90){
-            System.out.println("超棒");
+            System.out.println("A");
             System.out.println("平均為" +sum);
         }
-        else if(sum>=60){
-            System.out.println("lol");
+        else if(sum>=80&sum<=89){
+            System.out.println("B");
             System.out.println("平均為" +sum);
         }
+        else if(sum>=70&sum<=79){
+            System.out.println("C");
+            System.out.println("平均為" +sum);
+        }
+        else if(sum>=60&sum<=69){
+            System.out.println("D");
+            System.out.println("平均為" +sum);
+        }
+        
         else{
-            System.out.println("trash");
+            System.out.println("E");
             System.out.println("平均為" +sum);
         }
 	}
