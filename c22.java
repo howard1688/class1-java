54ca5d7bc60b1020a2334a230896c72857b96df0
howard1688 <111676522+howard1688@users.noreply.github.com>
2022-10-07 11:42:17 +0800

    Add files via upload

---
+++ c22.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class c22{
+	public static void main(String[] args){
+        int ii=9;
+        String space = "";
+        while(ii>=0){
+            System.out.printf("%d %n",ii);
+            ii--;
+            space=space+" ";
+            System.out.printf("%s",space);
+        }
+    }
+}
